f8834e066368696da5e807595419586abeabc194
jaykyle <lekylanviboko@gmail.com>
2022-11-20 15:11:13 +0700

    buid header, footer mobile

--- src/layout/AppFooter.tsx
+++ src/layout/AppFooter.tsx
@@ -2,54 +2,26 @@ import React from 'react';
 import Button from '../components/button/Button';
 import List from '../components/list/List';
 import SocialLink from '../components/social/SocialLink';
+import logo from 'src/assets/images/logo.png';
 
 const AppFooter = () => {
   return (
     <div role="contentinfo" className="app-footer">
-      <div className="footer-1">
-        <List title="3D Printing">
-          <List.ListItem>DMLS</List.ListItem>
-          <List.ListItem>FDM</List.ListItem>
-          <List.ListItem>PolyJet</List.ListItem>
-          <List.ListItem>SLS</List.ListItem>
-          <List.ListItem>Metal 3D Printing</List.ListItem>
-        </List>
-      </div>
-      <div className="footer-2">
-        <List title="Materials">
-          <List.ListItem>Metal</List.ListItem>
-          <List.ListItem>Photopolymers</List.ListItem>
-          <List.ListItem>Thermoplastic</List.ListItem>
-          <List.ListItem>Resin</List.ListItem>
-        </List>
-      </div>
-      <div className="footer-3">
-        <List title="Resources">
-          <List.ListItem>3D printing guide</List.ListItem>
-          <List.ListItem>Metal 3D printing guide</List.ListItem>
-          <List.ListItem>Case studies</List.ListItem>
-          <List.ListItem>Blog</List.ListItem>
-          <List.ListItem>FAQ</List.ListItem>
-          <List.ListItem>API</List.ListItem>
-        </List>
-      </div>
-      <div className="footer-4">
-        <List title="Company">
-          <List.ListItem>About us</List.ListItem>
-          <List.ListItem>Out Platform</List.ListItem>
-          <List.ListItem>Quality Assurance</List.ListItem>
-          <List.ListItem>Security</List.ListItem>
-          <List.ListItem>Privacy Policy</List.ListItem>
-          <List.ListItem>Terms of Service</List.ListItem>
-        </List>
-      </div>
-      <div className="footer-5">
-        <h4 className="list-title">Connect V1hub</h4>
-        <div className="icon">
-          <SocialLink icon="icofont-facebook" />
-          <SocialLink icon="icofont-youtube-play" />
-          <SocialLink icon="icofont-whatsapp" />
+      <div className="row">
+        <div className="col-12">
+          <img src={logo} alt="logo" style={{ width: 125 }} />
+          <p className="description-logo">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum dolor, fermentum
+            etiam nisl.
+          </p>
         </div>
+        <div className="col-12"></div>
+      </div>
+      <div className="footer-center">
+        <ul className="list list-horizontal">
+          <li className="list-item">Privacy Policy</li>
+          <li className="list-item">Terms of Services</li>
+        </ul>
       </div>
     </div>
   );
