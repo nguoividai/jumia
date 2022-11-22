43ec07712d1662f5c22c7f15f4d5fc30c2de0db4
jaykyle <lekylanviboko@gmail.com>
2022-11-22 15:26:14 +0700

    bo sung tab select size

--- src/components/tab/JumiaTab.tsx
+++ src/components/tab/JumiaTab.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Tab, Tabs } from 'react-bootstrap';
+import { Badge, Form, ListGroup, Tab, Tabs } from 'react-bootstrap';
 
 const JumiaTabs = () => {
   return (
     <>
       <Tabs defaultActiveKey="profile" className="jumia-tab mb-3">
         <Tab eventKey="home" title="Select Size">
-          Select Size
+          <div className="jumia-size">
+            <div className="size-title">
+              Choose a size
+              <Badge className="float-right danger">Required</Badge>
+            </div>
+            <div className="size-description">Select 1</div>
+          </div>
+          <div className="mt-1">
+            <ListGroup className="jumia-list-size" variant="flush">
+              <ListGroup.Item>
+                <Form.Check type="radio" name="size" label="Small" />
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Form.Check type="radio" name="size" label="Medium" />
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Form.Check type="radio" name="size" label="Large" />
+              </ListGroup.Item>
+            </ListGroup>
+          </div>
         </Tab>
         <Tab eventKey="profile" title=" Select Crust">
           Select Crust
